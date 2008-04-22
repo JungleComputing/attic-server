1b8ff3be5aaebfb4dedc6583d9a5bc9c95764244
Niels Drost <n.drost@esciencecenter.nl>
2008-04-22 16:03:21 +0000

    unused import version = 2.1.1

--- src/ibis/server/Server.java
+++ src/ibis/server/Server.java
@@ -7,7 +7,6 @@ import java.io.PrintStream;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
